0077bd8de09c56dd0b6ca1b721d34b0c158fd6e3
William Patrick <117779516+dimitri8421@users.noreply.github.com>
2024-02-15 20:14:48 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function criaCards() {
     let html = '';
     for (let i = 0; i < cards.length; i++) {
         let tipo = cards[i].link.match(/wa\.me/) ? "whatsapp" : (cards[i].link.match(/instagram/) ? "instagram" : "facebook");
-        html +=`<a class="fundo-blur d-flex align-items-end  p-3 position-relative gap-3 " href="${cards[i].link}"><div><h2>${cards[i].titulo}</h2><h3 class="text-end ">${cards[i].text}</h3></div><div class="imgs"><div data-icon="${tipo}"></div><img class="position-absolute img-social" src="/assets/img/${cards[i].image}" alt="${tipo}"></div></a>\n`;
+        html +=`<a class="fundo-blur d-flex align-items-end  p-3 position-relative gap-3 " href="${cards[i].link}"><div><h2>${cards[i].titulo}</h2><h3 class="text-end ">${cards[i].text}</h3></div><div class="imgs"><div data-icon="${tipo}"></div><img class="position-absolute img-social" src="assets/img/${cards[i].image}" alt="${tipo}"></div></a>\n`;
     }
     document.querySelector("#sociais").innerHTML = html;
     
